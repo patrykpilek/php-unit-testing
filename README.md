685bd80b245539a296a104e82ee99c373db840ec
Patryk Pilek <patryk.pilek@gmail.com>
2022-12-28 15:35:14 +0000

    test that incorrect results are not returned: using multiple test methods

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
   - [Installing PHPUnit](https://phpunit.de/getting-started/phpunit-9.html)
   - write and run your first test: an introduction to assertions
   - test a function: using multiple assertions
+  - [List of PHPUnit assertions](https://phpunit.readthedocs.io/en/9.5/assertions.html?highlight=assertions)
+  - test that incorrect results are not returned: using multiple test methods
 - [ ] Configuring the PHPUnit test environment
  
 - [ ] Test dependencies, fixtures and exceptions
--- tests/FunctionTest.php
+++ tests/FunctionTest.php
@@ -2,13 +2,19 @@
 
 use PHPUnit\Framework\TestCase;
 
+require '../functions.php';
+
 class FunctionTest extends TestCase
 {
+
     public function testAddReturnsTheCorrectSum()
     {
-        require '../functions.php';
-        
         $this->assertEquals(4, add(2, 2));
-        $this->assertEquals(8, add(3, 5));        
+        $this->assertEquals(8, add(3, 5));
+    }
+    
+    public function testAddDoesNotReturnTheIncorrectSum()
+    {
+        $this->assertNotEquals(5, add(2, 2));
     }    
 }
